f5e8c1be4ceb7f44227d66c15989d02bc100172b
Jan Pospíchal <jpospichalzsmn@seznam.cz>
2026-04-24 21:08:27 +0200

    Board with untachable borders

---
+++ Assets/Scripts/BorderScript.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class BorderScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("You touched the border and got infected by the virus!");
+    }
+}
