ec76542edc6d8a1c9e4864b17baba372658cacff
Jan Pospíchal <jpospichalzsmn@seznam.cz>
2026-04-26 12:33:26 +0200

    Fix old maze rotation script

--- Assets/Scripts/MazeCenteredScript.cs
+++ Assets/Scripts/MazeCenteredScript.cs
@@ -39,7 +39,7 @@ public class MazeCenteredScript : MonoBehaviour
     void Update()
     {
         if (_mazeRotatingCounter <= mazeRotatingTime)
-            transform.Rotate(Vector3.right * rotationSpeed);
+            transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
         
         _mazeRotatingCounter++;
         if (_mazeRotatingCounter >= rotatingCycleLength)
