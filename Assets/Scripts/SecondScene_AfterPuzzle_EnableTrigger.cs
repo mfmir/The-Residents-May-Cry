242a18ff9c12aab517a0f753768f897f1941ce32
Tomas Jan Bestak <tomas.bestak2001@gmail.com>
2026-04-26 12:42:45 +0200

    SEcond scene scripts

--- Assets/Scripts/SecondScene_AfterPuzzle_EnableTrigger.cs
+++ Assets/Scripts/SecondScene_AfterPuzzle_EnableTrigger.cs
@@ -1,16 +1,31 @@
+using DefaultNamespace;
 using UnityEngine;
 
 public class SecondScene_AfterPuzzle_EnableTrigger : MonoBehaviour
 {
+    bool finished_script = false;
+    [SerializeField] private TriggerDialogBox_STOP enableNextDialogScareTrigger;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!finished_script && SecondSceneData.minigameFinished)
+        {
+            enableNextDialogScareTrigger.gameObject.SetActive(true);
+            finished_script = true;
+            Debug.Log("Enableling final box_Start");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!finished_script && SecondSceneData.minigameFinished)
+        {
+            enableNextDialogScareTrigger.gameObject.SetActive(true);
+            finished_script = true;
+            //gameObject.SetActive(false);
+            Debug.Log("Enableling final box_Update");
+        }
     }
 }
