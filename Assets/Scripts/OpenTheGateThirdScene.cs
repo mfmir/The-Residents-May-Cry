b37fd2e73463c07c04bcdbcf0a9075d87593f2a1
Jan Pospíchal <jpospichalzsmn@seznam.cz>
2026-04-26 12:02:00 +0200

    Make third scene working again

---
+++ Assets/Scripts/OpenTheGateThirdScene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class OpenTheGateThirdScene : MonoBehaviour
+{
+    public float openingSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += Vector3.down * openingSpeed * Time.deltaTime;
+        if (gameObject.transform.position.y <= -18)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
