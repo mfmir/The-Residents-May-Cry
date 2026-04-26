205cfbe4935dcc62789cdcf5d17cfdbcfd764c73
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-04-26 15:14:21 +0200

    Add cursor for credits

--- Assets/Scripts/CreditQuitScript.cs
+++ Assets/Scripts/CreditQuitScript.cs
@@ -7,7 +7,8 @@ public class CreditQuitScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
